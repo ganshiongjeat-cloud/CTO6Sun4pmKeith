94c278f3c5ef41683108ae3ade2fe7c8732665dd
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-12 09:25:05 +0000

    Jul 12, 2026, 9:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -33,7 +33,7 @@ function draw() {
   // Challenge 5: A square face using quad() function
 ellipse(200,300,70,40)
 ellipse(800,300,70,40)
-rect(100,290,80,40)
+rect(150,290,80,40)
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
