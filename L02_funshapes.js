f48b855af360d1740da04099ffb920c6f6fd9197
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-12 09:00:46 +0000

    Jul 12, 2026, 9:00 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -25,7 +25,7 @@ function draw() {
   // Challenge 3: A house using rect() function
 rect(300,100,700,500)
 rect(600,450,100,180)
-circle(300,500,50,50)
+circle(700,500,50,50)
 rect(300,100,700,280)
   // Challenge 4: A rocketship using triangle() function
 
