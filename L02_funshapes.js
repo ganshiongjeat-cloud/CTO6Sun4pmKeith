9ea3924683af96312fdc4d44694ef6871202971e
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-12 08:34:03 +0000

    Jul 12, 2026, 8:34 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -16,7 +16,7 @@ function draw() {
   // to create a ellipse -> ellipse(X position, Y posotion, width, height)
 
   // Challenge 2: A face using circle() function
-CSSNumericValue()
+circle
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
