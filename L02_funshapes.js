515708c12ed4b3e3eae0ba0c8215c0a80540cdf6
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-12 09:27:12 +0000

    Jul 12, 2026, 9:27 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -35,7 +35,7 @@ ellipse(200,300,70,40)
 ellipse(800,300,70,40)
 rect(125,280,80,40)
 rect(800,280,80,40)
-rect(600,300,400,400)
+rect(200,200,400,400)
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
