fd850282bb204f8f0392d81df6045190a06017c3
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-12 08:22:51 +0000

    Jul 12, 2026, 8:22 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,6 +8,7 @@ function setup(){
 
 function draw() {
   ellipse(600, 300, 600, 300)
+  ellipse()
   // Challenge 1: An eye using ellipse() function
   // to create a ellipse -> ellipse(X position, Y posotion, width, height)
 
