f036c90dba6da83551d79280f19a510f6483f452
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-19 08:29:57 +0000

    Jul 19, 2026, 8:29 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,20 +6,20 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-  fill(200,0,0);
-  circle(75,200,100);
+  // fill(200,0,0);
+  // circle(75,200,100);
 
-  fill(0,200,0);
-  circle(225,200,100);
+  // fill(0,200,0);
+  // circle(225,200,100);
 
-  fill(0,0,200);
-  circle(375,200,100);
+  // fill(0,0,200);
+  // circle(375,200,100);
 
-  fill(200,200,200);
-  circle(525,200,100);
+  // fill(200,200,200);
+  // circle(525,200,100);
 
   for(let i =0; i<4; i++){
-    circle(75 + (i*50),200,100)
+    circle(75 + (i*150),200,100)
   }
   // Task 1: Colour Gradient
 
