4bd175256c6240685a3de105a40c4a1f7ccae73f
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-26 08:22:19 +0000

    Jul 26, 2026, 8:22 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -73,6 +73,6 @@ let startY = (height - totalwidth) / 2 + circlediameter / 2;
 for (let row = 0; row < numcircle; row++)
   let rowY = startY + (circlediameter * row);
 let x = startX + i * circlediameter;
-ellipse
+ellipse(x ,)
 }
 
