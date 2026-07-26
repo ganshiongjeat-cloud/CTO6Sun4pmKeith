9d6ec5133c795ad865edb9a5dcf17a89f644a02d
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-07-26 08:53:08 +0000

    Jul 26, 2026, 8:53 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -13,6 +13,7 @@ function draw(){
 function mousePressed(){
     for(let i = 0;i<shapecolour;i++)
     shapecolour = (random(255),random(255),random(255))
+    fill()
     circle(random(600),random(400),random(60))
     rect(random(600),random(400),random(100),random(80))
 }
