db3d3ce38021adb542d6dadba5dd2d9d16b99b65
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-16 08:20:44 +0000

    Aug 16, 2026, 8:20 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -24,6 +24,6 @@ function keyPressed(){
         let x = random(width);
         let y = random(height);
         let size = random(5,40);
-        fill(random(255),random())
+        fill(random(255),random(255),random(255));
     }
 }
