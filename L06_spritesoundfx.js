205e001b4a692c6a1cdefb451289231867f0605a
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-23 08:20:38 +0000

    Aug 23, 2026, 8:20 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -4,7 +4,7 @@ background(220);
 }
 
 function draw(){
-    circle(x,y,50);
+   
     if(keyCode === 38){
         y -= 5;}
     if(keyCode === 37){
@@ -16,5 +16,5 @@ function draw(){
     if(keyCode === 40){
         y += 5;
     }
-    
+ circle(x,y,50);
 }
