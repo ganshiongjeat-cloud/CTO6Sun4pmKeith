e2c603de8ddd8d77bd72a2ac93c6d710b1ad4bad
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-23 08:27:56 +0000

    Aug 23, 2026, 8:27 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -4,7 +4,7 @@ let soundEffect, bgmusic, staticImage;
 function preload(){
     soundEffect = loadSound('assets/pop.mp3');
     bgMusic = loadSound('assets/bossaNova.mp3');
-    staticImage = loadImage('assets/')
+    staticImage = loadImage('assets/pico-a.png')
 }
 function setup(){
 createCanvas(400,400);
