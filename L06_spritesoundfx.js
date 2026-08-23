f832374e116aa0bf79ffbbc434f037c9075b010f
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-23 08:48:30 +0000

    Aug 23, 2026, 8:48 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,6 +1,7 @@
 let x = 200
 let y = 200
 let soundEffect, bgmusic, staticImage;
+let staticImageX
 function preload(){
     soundEffect = loadSound('assets/pop.mp3');
     bgMusic = loadSound('assets/bossaNova.mp3');
