7cebab274a5afcfb254179574d49c91f5736adb3
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-23 09:03:09 +0000

    Aug 23, 2026, 9:03 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -17,7 +17,9 @@ bgMusic.loop();
 function draw(){
 background("lightblue");
 if(staticImageX <=50 || staticImageX >=300
-    || staticImageY <=50 || staticImageY)
+    || staticImageY <=50 || staticImageY >=300){
+        
+    }
 if(keyIsDown(LEFT_ARROW)){
     staticImageX -= staticSpeed;
 }
