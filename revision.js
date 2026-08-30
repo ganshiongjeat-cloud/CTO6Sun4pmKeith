85b751d2f50c5888c82cc7007f015a867c7579ff
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-30 09:28:40 +0000

    Aug 30, 2026, 9:28 AM

--- revision.js
+++ revision.js
@@ -14,4 +14,20 @@ for(let i =0; i<10; i++){
     circle(30*i+20,30 + i*20,20)
 }
 }
-
+/*revision notes
+css -> cascading style sheet
+heading in html -> h1 to h7
+must have in html -> body, title, html, head
+declare a variable -> let variable_name = value;
+must have functions in p5js -> setup() and draw()
+in for loop be very careful of the initial variable declaration
+for loop syntax -> for(let i =0; i<5; i++)
+for loop syntax -> for(declaration; condition; increment)
+to create new canvas -> createCanvas(width,height)
+cordinate system -> top left is (0,0)
+if move right -> x increase y remain same 
+cordinate system -> if move down x same y increase
+coordinate system-> we dont use negative value
+functiion that only run once -> function setup()
+function that run multiple times-> function draw()
+*/
