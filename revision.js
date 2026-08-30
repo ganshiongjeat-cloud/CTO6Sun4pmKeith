427bc6d3a6d4c4b369c82700dbd88ecbc09297e9
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-08-30 09:11:11 +0000

    Aug 30, 2026, 9:11 AM

--- revision.js
+++ revision.js
@@ -5,7 +5,7 @@ function setup(){
 function draw(){
     stroke(255,0,0);
     strokeWeight(10);
-    triangle(200,150,250,250,160,250);
+    triangle(200,150,240,250,160,250);
     fill(0);
     circle(300,200,100);
     rect(350,150,100,100);
