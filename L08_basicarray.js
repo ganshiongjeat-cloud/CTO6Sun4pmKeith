bdf34f85b2228f9f54f71d8a9fbf658f462fde86
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-13 09:04:33 +0000

    Sep 13, 2026, 9:04 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -9,8 +9,8 @@ function setup(){
     createCanvas(600,400);
     
 // }
-// function draw(){
-//     background(220);
+function draw(){
+    background(220);
 //     fill(random(255),random(255),random(255));
 //     rect(x,y,100);
 //     x += speed
@@ -30,5 +30,4 @@ yPos-=0.6;
 
 }
 
-
-
+}
