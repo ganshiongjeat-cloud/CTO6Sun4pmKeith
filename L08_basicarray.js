9b817b15dc4fbcc03e4a1b4047ceaf8ab1e316de
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-13 08:45:00 +0000

    Sep 13, 2026, 8:45 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -26,7 +26,7 @@ function setup(){
     text("your age" ,50,100);
     text("your favourite activity" ,50,150);
     for(let x = 0;x<5;x++){
-        text("x" , 50,50 );
+        text("x" , 50,(50 + i*50) );
     }
 
 }
