99a9c0a742f79f75c5c57caed802e4e7ada27147
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-13 09:09:10 +0000

    Sep 13, 2026, 9:09 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -26,7 +26,7 @@ function draw(){
 textSize(32);
 
     for(let i = 0;i<favfood.length; i++){
-        text((i+1)+"."+favfood[i] , 50,yPos+(50 + i*50) );
+        text((i+1)+"."+favfood[i] , 50,yPos+(50 + i*lineGap) );
     }
 
 
