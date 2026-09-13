8a241226850e997683bfd5e0f0b128bd09af8852
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-13 08:11:49 +0000

    Sep 13, 2026, 8:11 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,7 +10,7 @@ function draw(){
     fill(random(255),random(255),random(255));
     rect(x,y,100);
     x += speed
-if(x + 100 >= width || x - 100 <=0){
+if(x + 50 >= width || x - 50 <=0){
         x =- speed;}
 }
 
