5e18e2e906ac9b706b5f061a56e621b7e64a8b8b
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-20 08:12:44 +0000

    Sep 20, 2026, 8:12 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,5 +1,6 @@
 let x = 100
 let y = 100
+let speed = 3
 function setup(){
     createCanvas(600,400);
     background(220)
@@ -9,5 +10,5 @@ function draw(){
     textSize(40)
     textalign(CENTER)
     text("Bounce!",x,y)
-    
+    x + speed
 }
