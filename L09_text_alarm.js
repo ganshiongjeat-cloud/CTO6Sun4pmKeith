9b1a4857fac30c90ec37fd55f2f8607c3a5c4050
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-20 08:32:20 +0000

    Sep 20, 2026, 8:32 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -20,5 +20,7 @@ function draw(){
     let h = hour();
     let m = minute();
     let s = second();
-    let string = nf(h,2)+" : " + nf(m,2)+" : " + nf(s,2)
+    let string = nf(h,2)+" : " + nf(m,2)+" : " + nf(s,2);
+    textsize(32);
+
 }
