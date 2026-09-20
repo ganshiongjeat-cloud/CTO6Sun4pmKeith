a91ad97cb3d6e623efca13552b21632576e0661c
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-20 08:54:41 +0000

    Sep 20, 2026, 8:54 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -49,7 +49,7 @@ function updateCountdown(){
         clearInterval(interval);
     }
     if (countdown < 1){
-        fill(random(255),random(255),random(255))
+        background(random(255),random(255),random(255))
         bgMusic.play();
     }
 }
