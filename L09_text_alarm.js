9d761263cd2b00bc38af66452a773c34315e4517
ganshiongjeat-cloud <gan_shiong_jeat@students.edu.sg>
2026-09-20 08:45:37 +0000

    Sep 20, 2026, 8:45 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -31,7 +31,7 @@ function draw(){
     text("Click me to start timer", width / 2, height = 50);
 
     textSize(100)
-    text(countdown, width / 2, height / 2);
+    text(countdown, 300, 200);
     
 }
 function mousePressed(){
